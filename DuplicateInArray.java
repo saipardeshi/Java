f793ff11e60cad99d7f12eed77b6d6189ac14fff
Sai Pardeshi <pardeshisai12@gmail.com>
2026-07-16 00:45:55 +0530

    Add DuplicateInArray program

--- DuplicateInArray.java
+++ DuplicateInArray.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DuplicateInArray {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 2, 5, 6, 3, 7, 8, 5};
+
+        Set<Integer> unique = new HashSet<>();
+
+        Set<Integer> duplicates = Arrays.stream(arr)
+                .boxed()
+                .filter(num -> !unique.add(num))
+                .collect(Collectors.toSet());
+
+        System.out.println("Duplicate Elements: " + duplicates);
+    }
 }
