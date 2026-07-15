3539caedcdf5382d91ef5ddc08f9a3a95cfe1dd3
Sai Pardeshi <pardeshisai12@gmail.com>
2026-07-16 00:45:37 +0530

    Add ReverseArray program

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class ReverseArray {
+  public static void main(String[] args){
+      int[] arr ={12,13,14,15,16};
+      int start=0;
+      int end=arr.length -1;
+
+      while(start <end ){
+          int temp =arr[start];
+          arr[start]=arr[end];
+          arr[end]=temp;
+
+          start++;
+          end--;
+      }
+      System.out.println("reversed array");
+
+      for(int num:arr){
+          System.out.print(num + " ");
+      }
+  }
+
 }
+
