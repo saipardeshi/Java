9de794702f1194afd095b31a4d6505785e33dc74
Sai Pardeshi <pardeshisai12@gmail.com>
2026-07-21 00:01:24 +0530

    added practice questions

---
+++ Launch2.java
@@ -0,0 +1,19 @@
+public class Launch2 {
+    public static void main(String[] args) {
+
+//        System.out.println("Name: Sai Pardeshi");
+//        System.out.println("Age :18");
+//        System.out.println("City:Pune");
+//            System.out.println("*****");
+//            System.out.println("*****");
+//            System.out.println("*****");
+//            System.out.println("*****");
+//            System.out.println("*****");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Launch4.java
@@ -0,0 +1,7 @@
+public class Launch4 {
+    public static void main(String[] sai){
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+    }
+}
