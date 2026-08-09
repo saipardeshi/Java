79589de4a55fb45c1070dd9ce9b854a367a7c398
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-09 20:35:24 +0530

    added voting eligibility program

--- Votingeligibility.java
+++ Votingeligibility.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Votingeligibility {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter age:");
+        int age =sc.nextInt();
+
+        if(age>=18){
+            System.out.println("Eligible to vote");
+        }else{
+            System.out.println("not eligible");
+        }
+    }
 }
