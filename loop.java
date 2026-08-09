087022d0ae0f497589e4c315dfc6d0480a31bd18
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-09 22:03:44 +0530

    add practice of java

---
+++ loop.java
@@ -0,0 +1,21 @@
+public class loop {
+    public static void main(String[] args) {
+//        for(int i=1;i<=5;i=i+1){
+//            System.out.println("sai");
+//        }
+//        for (int count =1;count <=6; count=count+2){
+//            System.out.println(count);
+//        }
+//        for (int count =2;count <=20; count=count+2){
+//            System.out.println(count);
+//        }
+//        for(int i=1;i<=10;i++){
+//            System.out.println(2*i);
+//            }
+        int n=10;
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+        }
+    }
+
