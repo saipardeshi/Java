fae4444204d2fe5dc33ecaeb75350d518833b244
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-19 15:21:13 +0530

    added Practice of java

---
+++ Demo.java
@@ -0,0 +1,21 @@
+public class Demo {
+    public static void main(String[] args){
+//        System.out.println("hello");
+
+//        interger ->byte short int long
+        byte b=7;
+        short s=10;
+        int i =1000;
+        long l=10000;
+//real numbers
+        float f=10.54f;
+        double d=23.0987;
+//        character
+      char c='a';    /*integer -> binary-> store*/
+//boolean
+        boolean bool=false;
+
+        System.out.println("interger values:"+b+","+s+","+i+","+l);
+        System.out.println("real values:"+f+","+d);
+    }
+}
