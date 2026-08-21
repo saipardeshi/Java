dd4ec5ef62f5d18a54c31a818ef1877ad3094c3a
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-21 21:52:11 +0530

    feat: add palindrome and array/string practice programs

---
+++ PalindromeCheck2.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class PalindromeCheck2 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter a word :");
+        String str = sc.nextLine();
+
+        String rev="";
+        for (int i=str.length()-1;i>=0;i--){
+            rev=rev+str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a palindrome");
+        }
+        sc.close();
+
+    }
+}
