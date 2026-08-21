7934c11e336c42244755af3642cb528e92095a4f
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-21 21:58:41 +0530

    feat: add swap, second largest and string reverse practice programs

---
+++ Practice37.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Practice37 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swap: a = " + a + ", b = " + b);
+
+        sc.close();
+    }
+}
