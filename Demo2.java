6075433f1d828a74f5ac7529a4cbb189bff6b235
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-22 23:52:20 +0530

    practice of java basic

---
+++ Demo2.java
@@ -0,0 +1,17 @@
+public class Demo2 {
+    public static void main(String[] args) {
+//        Loops -while,do while,for loop
+
+        int i=1;
+//        int i=10;
+//        while(i>=1){
+//            System.out.println(i);
+//            i--;
+//        }
+
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
