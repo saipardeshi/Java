d9d3c5191f3e233457e7137a8aafe02f54ffafea
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-22 23:50:03 +0530

    Swap the number with temp variable

---
+++ Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+    public static void main(String[] args) {
+         int a=10;
+         int b=20;
+        System.out.println("Before swap:a="+a+",b="+b);
+        int temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println("Before swap:a="+a+",b="+b);
+
+    }
+}
