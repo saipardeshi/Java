178483750faa71de909cc16d6ddcf809dd4fb380
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-24 00:16:57 +0530

    Added pratice of armstrong code

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 
+
 public class ArmstrongNumber {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-
-        System.out.println("Enter a number :");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to check the number is armstrong num or not ");
         int num=sc.nextInt();
 
         int originalNum =num;
-        int sum =0;
+        int sum=0;
 
-        while(num>0){
-            int digit =num % 10;
-            sum=sum+(digit * digit * digit);
-            num=num/10;
-        }if(sum == originalNum){
-            System.out.println(originalNum +" is a armstrong number ");
-
+        while(num>0) {
+            int digit = num % 10;
+            sum = sum + (digit * digit * digit);
+            num = num / 10;
+        }
+        if(sum == originalNum){
+            System.out.println(originalNum +"num is armstrong");
         }else{
-            System.out.println(originalNum +" is not a armstrong number ");
+            System.out.println(originalNum+"num is not armstrong");
         }
         sc.close();
     }
+
 }
