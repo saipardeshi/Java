7bfaf0795a2b200743de08facce0037c381e684b
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-25 23:55:24 +0530

    Add string reversal using two-pointer swap

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "hello";
+        char[] chars = str.toCharArray();
+        int left = 0, right = chars.length - 1;
+
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        System.out.println("Reversed: " + new String(chars));
+    }
+}
