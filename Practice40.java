d97d481eaea4bd8401f973a4c945637c5e6e46ad
Sai Pardeshi <pardeshisai12@gmail.com>
2026-08-28 19:26:05 +0530

    Add print multiplication table of 40

---
+++ Practice40.java
@@ -0,0 +1,9 @@
+public class Practice40 {
+    public static void main(String[] args) {
+        int num = 40;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
